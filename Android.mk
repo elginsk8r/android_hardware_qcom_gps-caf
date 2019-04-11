aff3b6bc8ce4118f8156c0d8bd12f1ed6d8279c8
Roger Liao <rogerliao@google.com>
2019-04-12 03:31:08 +0800

    gps: use namespaces to control access to qcom projects
    Change-Id: Ib0784cf215fd745703a83446b14b4cd43f27dc58

--- Android.mk
+++ Android.mk
@@ -1,5 +1,9 @@
+ifneq ($(filter $(call my-dir),$(PRODUCT_SOONG_NAMESPACES)),)
+
 ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/build/target_specific_features.mk
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
+
 endif
